58384d3f65edfefa8301bdba43dd01105ceb2850
Azee <aziztitu1996@gmail.com>
2018-04-22 19:41:15 -0500

    Makes MiniMap to rotate as player rotates around Y-axis.

---
+++ Assets/Scripts/Azee/MiniMapCameraMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapCameraMovement : MonoBehaviour
+{
+
+    GameObject playerGameObject;
+
+	// Use this for initialization
+	void Start () {
+		playerGameObject = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		focusOnPlayer();
+	}
+
+    void focusOnPlayer()
+    {
+        transform.position = new Vector3(playerGameObject.transform.position.x, transform.position.y, playerGameObject.transform.position.z);
+
+        Vector3 moddedForward = playerGameObject.transform.forward;
+        moddedForward.y = 0;
+
+        float playerForwardAngle = Vector3.SignedAngle(Vector3.forward, moddedForward, Vector3.up);
+
+        Vector3 newEulerAngle = new Vector3(transform.eulerAngles.x, playerForwardAngle, transform.eulerAngles.z);
+        transform.eulerAngles = newEulerAngle;
+    }
+}
