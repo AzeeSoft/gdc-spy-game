1816b39c42c73828c9c57fee97469759e1daff8e
Unknown <xylorhksshadow.sw@gmail.com>
2018-04-23 19:21:37 -0500

    Adds Door Anim and Script

---
+++ Assets/Animations/Door Move/DoorScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    private Animator _animator;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _animator = GetComponent<Animator>();
+        _animator.SetBool("open", false);
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            _animator.SetBool("open", true);
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    { 
+       _animator.SetBool("open", false);   
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
