582a74e9e7012a143f09ab1b04a4601f6f791b85
Kris J <KrisJohnson458@gmail.com>
2018-05-25 16:47:04 -0500

    Script for color change and minimap
    Added two script files for functionality for guard color change and minimap toggle

---
+++ Assets/Scripts/Kris/ColorChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorChanger : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void Start()
+    {
+        Renderer renderer = GetComponent<Renderer>();
+        Material mat = renderer.material;
+
+        float emission = Mathf.PingPong(Time.time, 1.0f);
+        Color baseColor = Color.red;
+
+        Color finalColor = baseColor; //* Mathf.LinearToGammaSpace(emission);
+
+        mat.SetColor("_EmissionColor", finalColor);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
