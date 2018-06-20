047072180fbff390f616a1f50ec3c893b9f1f564
Azee <aziztitu1996@gmail.com>
2018-06-20 13:43:15 -0500

    Adds XRayVision Battery

--- Assets/Scripts/Azee/Player/VisionToggler.cs
+++ Assets/Scripts/Azee/Player/VisionToggler.cs
@@ -2,26 +2,97 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VisionToggler : MonoBehaviour {
+public class VisionToggler : MonoBehaviour
+{
+    public float MaxBattery = 100f;
+    public float depletionTime = 3f;
+    public float rechargeTime = 6f;
+
+    [SerializeField] [ReadOnly] private float _currentBattery = 100f;
+    [SerializeField] [ReadOnly] private float _minRequiredBattery = 10f;
 
     private Camera fpsCamera;
     private XRayVision xRayVision;
 
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         fpsCamera = GetComponentInChildren<Camera>();
         xRayVision = fpsCamera.GetComponent<XRayVision>();
     }
 
-    void LateUpdate () {
+    void Update()
+    {
+        UpdateBattery();
+    }
+
+    void LateUpdate()
+    {
         CheckVisionToggle();
+    }
+
+    void UpdateBattery()
+    {
+        if (xRayVision.enabled)
+        {
+            DepleteBattery();
+            if (_currentBattery <= 0)
+            {
+                DisbleXRayVision();
+            }
+        }
+        else
+        {
+            RechargeBattery();
+        }
+    }
+
+    void DepleteBattery()
+    {
+        if (_currentBattery > 0)
+        {
+            _currentBattery -= (MaxBattery / depletionTime) * Time.deltaTime;
+
+            if (_currentBattery < 0)
+            {
+                _currentBattery = 0;
+            }
+        }
+    }
+
+    void RechargeBattery()
+    {
+        if (_currentBattery < MaxBattery)
+        {
+            _currentBattery += (MaxBattery / rechargeTime) * Time.deltaTime;
+
+            if (_currentBattery > MaxBattery)
+            {
+                _currentBattery = MaxBattery;
+            }
+        }
     }
 
     void CheckVisionToggle()
     {
-        if (Input.GetButtonDown("Toggle Vision") && xRayVision)
+        if (Input.GetButtonDown("Toggle Vision") && xRayVision && _currentBattery >= _minRequiredBattery)
         {
-            xRayVision.enabled = !xRayVision.enabled;
+            ToggleVision();
         }
+    }
+
+    void ToggleVision()
+    {
+        xRayVision.enabled = !xRayVision.enabled;
+    }
+
+    void EnableXRayVision()
+    {
+        xRayVision.enabled = true;
+    }
+
+    void DisbleXRayVision()
+    {
+        xRayVision.enabled = false;
     }
 }
