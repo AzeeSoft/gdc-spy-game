a4af89cb0096868f1da6fefb22f7c7d90762491f
Azee <aziztitu1996@gmail.com>
2018-06-21 19:44:35 -0500

    Adds LazerTrap. Adds Extra Collider for Player to detect Lazer collision.

---
+++ Assets/Scripts/Azee/Environment/Traps/LazerTrap.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LazerTrap : MonoBehaviour
+{
+    public float LoopTime = 1f;
+    public GameObject LazerBeamsGameObject;
+
+    private Animator _animator;
+    private LazerBeamCollisionDetector _lazerBeamCollisionDetector;
+
+    private bool _wasOpen = true;
+    private bool _isOpen = true;
+
+    void Awake()
+    {
+        _animator = GetComponent<Animator>();
+        _lazerBeamCollisionDetector = LazerBeamsGameObject.AddComponent<LazerBeamCollisionDetector>();
+        _lazerBeamCollisionDetector.Initialize(this);
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        StartCoroutine(LoopBeamOpenClose());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_wasOpen != _isOpen)
+        {
+            _wasOpen = _isOpen;
+            _animator.SetTrigger(_isOpen ? "open" : "close");
+        }
+    }
+
+    IEnumerator LoopBeamOpenClose()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(LoopTime);
+
+            _isOpen = !_isOpen;
+        }
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.CompareTag("Guard"))
+        {
+            _isOpen = false;
+        }
+    }
+
+    private void OnLazerBeamCollisionDetected(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Debug.Log("Player hit by Lazer");
+            // TODO: Do whateva
+        }
+    }
+
+
+    public class LazerBeamCollisionDetector : MonoBehaviour
+    {
+        private LazerTrap _lazerTrap;
+
+        public void Initialize(LazerTrap lazerTrap)
+        {
+            _lazerTrap = lazerTrap;
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+//            Debug.Log("Lazer Beam Collision");
+//            Debug.Log(collision.collider.gameObject);
+            _lazerTrap.OnLazerBeamCollisionDetected(collision);
+        }
+    }
+}
