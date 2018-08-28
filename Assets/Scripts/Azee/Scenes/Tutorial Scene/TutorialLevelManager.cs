d1617240d5811922baab4e7abac909bf81c18b2a
Azee <aziztitu1996@gmail.com>
2018-08-28 01:09:59 -0500

    Adds tutorials. Updates Player Prefab. Updates Security Camera.

---
+++ Assets/Scripts/Azee/Scenes/Tutorial Scene/TutorialLevelManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialLevelManager : LevelManager {
+
+	// Use this for initialization
+	void Start () {
+		TutorialManager.Instance.ShowTutorial("Initializing");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetButtonDown("Submit"))
+	    {
+            TutorialManager.Instance.BroadcastTutorialAction("ok");
+	    }
+	}
+}
---
+++ Assets/Scripts/Azee/Tools/Tutorials/BroadcastOnTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class BroadcastOnTrigger : MonoBehaviour
+{
+    public string OtherTag = "Player";
+    public string BroadcastAction = "Triggered";
+
+    public UnityEvent OnBroadcast;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag(OtherTag))
+        {
+            TutorialManager.Instance.BroadcastTutorialAction(BroadcastAction);
+            if (OnBroadcast != null)
+            {
+                OnBroadcast.Invoke();
+            }
+        }
+    }
+}
