db96e63829deeda36cd6842e5d9a85fbc85dd5f6
Azee <aziztitu1996@gmail.com>
2018-08-27 20:15:34 -0500

    Links the xray battery to the UI

---
+++ Assets/Scripts/Azee/Player/PlayerHUDController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHUDController : MonoBehaviour {
+
+    [Serializable]
+    public class UIElementsModel
+    {
+        [CanBeNull] public Image XRayUI;
+        [CanBeNull] public Image HealthUI;
+    }
+
+    public UIElementsModel UIElements;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
